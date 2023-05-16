7bb96ab7d6dcf57200617ef94b0e06a8bfdb90cb
eunjin <eunjin.kang22@gmail.com>
2023-05-16 19:05:16 +0900

    작업중

--- tree_test/tree.py
+++ tree_test/tree.py
@@ -1,1 +1,8 @@
-print("file tree fork test_ej")
+import shotgun_api3
+
+url = "https://rndtest.shotgrid.autodesk.com/"
+script_name = "dongjin"
+api_key = "n2khFoc&jybfpufdwabdzgxyr"
+
+sg = shotgun_api3.Shotgun(url, script_name, api_key)
+
