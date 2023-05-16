511bb1801676a7ddac034f7a31b03bbfc27a10bf
Park Sung Woo <west@localhost.localdomain>
2023-05-16 19:06:01 +0900

    sg test

--- python_sw/file_tree.py
+++ python_sw/file_tree.py
@@ -1,3 +1,4 @@
+# -*- coding:utf-8 -*-
 
 import shotgun_api3
 import PySide2
@@ -15,22 +16,68 @@ sg = shotgun_api3.Shotgun(SERVER_PATH,
 #
 # project = sg.create("Project", project_data)
 
-usr_project_name = "SW_Project_test_4"
-
+usr_project_name = "SW_Project_test_3"
+project_data = {"name": usr_project_name, "sg_description": "This is a new project."}
+usr_shot_name = "SH003"
 
 def create_project():
-
-    project_data = {"name": usr_project_name, "sg_status": "Active", "sg_description": "This is a new project."}
-
+    # project_data = {"name": usr_project_name, "sg_description": "This is a new project."}
     project = sg.create("Project", project_data)
     print("Created project: {}".format(project))
+    return project
 
-    """ if project already exist error """
 
+def create_shot():
+    shot_data = {"code": "test", "Project": ["name", "is", usr_project_name]}
+    shot = sg.create("Asset", shot_data)
+    print(shot)
+
+
+def get_all_project():
+    """all project"""
+    projects = sg.find("Project", [], ["name"])
+    for project in projects:
+        return sorted(list(set([project["name"] for project in projects])))
+
+
+a = get_all_project()
+print(a)
+
+""" if project already exist error """
+if usr_project_name not in a:
+    create_project()
+
+else:
+    print("{} : Project already exist".format(usr_project_name))
+
+
+project_dict = sg.find_one("Project", [["name", "is", usr_project_name]], ["id"])
+print(project_dict)
+# print(tt["id"])
+shot_data = {
+    "project": project_dict,
+    "code": usr_shot_name
+}
+
+# sg.create("Shot", shot_data)
+shot_filters = [
+    ['project', 'is', project_dict]
+]
+
+get_all_shot = sg.find("Shot", shot_filters, ["id", "code"])
+print(get_all_shot)
+
+
+
+
+
+
+
+# ========================================================
 
 def get_active_project():
     """
-    active 된 프로젝트 목록을 get 하는 함수이다.
+    active 된 프로젝트 get 하는 함수이다.
     """
     projects = sg.find("Project", [["sg_status", "is", "Active"]], ["name"])
     # pprint(projects)
@@ -40,16 +87,12 @@ def get_active_project():
         return sorted(list(set([project["name"] for project in projects])))
 
 
+def add_shot():
+    fields = ["code", "sg_status_list"]
+    filters = [['id', 'in']]
+    shots = sg.find("Shot", filters, fields)
+
+
 def set_file_tree():
     print("set file tree")
-
-
 # set_file_tree()
-a = get_active_project()
-print(a)
-
-if usr_project_name not in a:
-    create_project()
-else:
-    print("{} : Project already exist".format(usr_project_name))
-
