305f6a56ef4ee6c4986ac8abac968cf6dc0e8094
eunjin <eunjin.kang22@gmail.com>
2023-05-17 18:44:51 +0900

    openpyxl, create dir

--- tree_test/tree.py
+++ tree_test/tree.py
@@ -1,8 +1,72 @@
-import shotgun_api3
+# import shotgun_api3
+#
+#
+# url = "https://rndtest.shotgrid.autodesk.com/"
+# script_name = "dongjin"
+# api_key = "n2khFoc&jybfpufdwabdzgxyr"
+# sg = shotgun_api3.Shotgun(url, script_name, api_key)
 
-url = "https://rndtest.shotgrid.autodesk.com/"
-script_name = "dongjin"
-api_key = "n2khFoc&jybfpufdwabdzgxyr"
+import os
+import openpyxl
 
-sg = shotgun_api3.Shotgun(url, script_name, api_key)
+
+# from dir_names import *  # Imports the list of folders from dir_names.py file
+
+main_dir = '/home/west/test/show'
+root_dir = 'goguma' #project name(code)
+main_dir_names = ['Gryffindor', 'Slytherin', 'Hufflepuff', 'Ravenclaw']  # Name of the sub-directories
+
+
+def create_project():
+    # shotgrid project code get ?
+    os.makedirs(os.path.join(main_dir, root_dir), exist_ok=True)
+
+
+def default_dir():
+    path = os.path.join(main_dir, root_dir)
+    default_directory = ['assets', 'sequences', 'tmp']
+    for d in default_directory:
+        os.makedirs(os.path.join(path, d), exist_ok=True)
+
+    os.makedirs(os.path.join(path, 'production'), exist_ok=True)
+
+    p = os.path.join(path, 'production')
+    directory = ['scan', 'excel']
+    for d in directory:
+        os.makedirs(os.path.join(p, d), exist_ok=True)
+
+
+def get_info():
+    wb = openpyxl.load_workbook('/home/west/test/show/goguma/production/excel/230517.xlsx')
+    sheet = wb['Shot']
+    a = sheet['A']
+    print(a)
+
+# def main():
+#     # Create directory
+#     for i in range(0, len(main_dir)):
+#         for j in range(0, len(main_dir[i])):
+#             dirName = str(root_dir) + '/' + str(main_dir_names[i]) + '/' + str(main_dir[i][j])
+#
+#             try:
+#                 # Create target Directory
+#                 os.makedirs(dirName)
+#                 print("Directory ", dirName, " Created ")
+#             except FileExistsError:
+#                 print("Directory ", dirName, " already exists")
+#
+#                 # Create target Directory if don't exist
+#             if not os.path.exists(dirName):
+#                 os.makedirs(dirName)
+#                 print("Directory ", dirName, " Created ")
+#             else:
+#                 print("Directory ", dirName, " already exists")
+
+
+if __name__ == '__main__':
+    # main()
+    # create_project()
+    # default_dir()
+    get_info()
+
 
