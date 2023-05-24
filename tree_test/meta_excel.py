16c75eadd6e5de0908c33a6dbcc3886a384ab7a7
Eunjin <eunjin.kang22@gmail.com>
2023-05-25 00:30:32 +0900

    excel test

--- tree_test/meta_excel.py
+++ tree_test/meta_excel.py
@@ -4,6 +4,8 @@ from openpyxl import Workbook
 from openpyxl.drawing.image import Image
 # from openpyxl.styles.fonts import Font
 
+import OpenEXR
+
 
 # class CreateExcel:
 #     def __init__(self) -> None:
@@ -121,3 +123,22 @@ for i, img_file in enumerate(img_file_list):
     ws.cell(row=i + 2, column=2, value=img_file)  
     
 wb.save("C:/Users/jin91/PipelineTD/git/test2.xls")
+
+
+# EXR 파일 경로
+# exr_file_path = "/show/seine/production/scan/20221017_plate_scan/002_A130C005_220226_RPGF/A130C005_220226_RPGF.0001014.exr"
+exr_file_path = "/show/seine/production/scan/20221017_plate_scan/002_A130C005_220226_RPGF/A130C005_220226_RPGF.0001014.exr"
+
+# EXR 파일 열기
+exr_file = OpenEXR.InputFile(exr_file_path)
+
+# 메타데이터 가져오기
+header = exr_file.header()
+
+# 모든 메타데이터 출력
+print("test:")
+for key, value in header.items():
+    print(f"{key}: {value}")
+
+# 파일 닫기
+exr_file.close()
